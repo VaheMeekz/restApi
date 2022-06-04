468cd8ae9a94d68ab1f9f66f790f95b342bfa784
VaheMeekz <vahe.mkrtchyan.a3@gmial.com>
2022-06-04 11:36:59 +0400

    env configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+    const app = await NestFactory.create(AppModule);
+    await app.listen(process.env.PORT || 5000, () => {
+        console.log(`Server Started On Port ${process.env.PORT}`)
+    });
 }
+
 bootstrap();
