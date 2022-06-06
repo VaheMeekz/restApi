fb14ec98fa8ab36fb4cc9db55d366d03b646cbf4
VaheMeekz <vahe.mkrtchyan.a3@gmial.com>
2022-06-06 13:21:19 +0400

    dto validations

--- src/users/dto/createUser.dto.ts
+++ src/users/dto/createUser.dto.ts
@@ -1,14 +1,19 @@
 import {ApiProperty} from "@nestjs/swagger";
+import {IsEmail, IsString, Length} from "class-validator";
 
 export class CreateUserDto{
     @ApiProperty({
         example:'example@gmail.com',
         description:"User email"
     })
+    @IsString({message:"Must be string"})
+    @IsEmail({},{message:"Incorect email"})
     readonly email:string;
     @ApiProperty({
         example:'123456',
         description:"User password"
     })
+    @IsString({message:"Must be string"})
+    @Length(4,8,{message:"Password length must me 4-8"})
     readonly password:string;
 }
